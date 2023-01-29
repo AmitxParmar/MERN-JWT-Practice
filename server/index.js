52403f944cb4a7971135a124998e514a3930d4f9
AmitxParmar <amitparmar901@gmail.com>
2023-01-30 00:24:48 +0530

    setup mongoose and create schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 
 console.log('starting.....server...');
-const app = express();
-app.get('/hello', (req, res) => {
-    res.send('hello world')
+
+app.use(cors());
+app.use(json());
+
+mongoose.connect("mongodb://localhost:27017/full-mern-stack-auth")
+
+app.post('/api/register', async (req, res) => {
+    res.json({ status: 'ok' })
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+
+    }
+    res.json({ status: 'ok' })
 })
 
 app.listen(8000, () => {
