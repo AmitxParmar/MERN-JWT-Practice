9e4a7da756e3a7e0c9d9469788eb537f3a051b46
AmitxParmar <amitparmar901@gmail.com>
2023-02-04 20:28:52 +0530

    finish dashboard

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const User = new mpngoose.Schema(
+const User = new mongoose.Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
